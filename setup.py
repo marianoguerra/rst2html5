a37eb652f61f5627cc63b2d784ff93e36396d7ee
Martín Gaitán <gaitan@gmail.com>
2014-09-15 11:37:28 -0300

    package as rst2html5-tools

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
-
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup
 
 setup(
-    name='rst2html5',
-    version='0.1',
+    name='rst2html5-tools',
+    version='0.2',
     author='Mariano Guerra',
     author_email='luismarianoguerra@gmail.com',
     url='https://github.com/marianoguerra/rst2html5',
     long_description=open('README.rst').read(),
     packages=['html5css3'],
-    scripts = ['bin/rst2html5'],
-    include_package_data = True,
+    package_data={'html5css3': ['thirdparty/*/*.*']},
+    include_package_data=True,
     install_requires=['docutils'],
+    entry_points={
+        'console_scripts': [
+            'rst2html5 = html5css3.main:main',
+        ]},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -26,4 +34,3 @@ setup(
         'Topic :: Utilities',
     ],
 )
-
